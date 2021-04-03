35e5c7d9f724f6a533ca0ba603e4fd21a6117e7f
Adem68 <Adem68@users.noreply.github.com>
2021-04-03 09:19:41 +0000

    deploy: 7eb8d7a807b390dce4a57bb54085135ca461a6e6

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,7 +14,7 @@ const RESOURCES = {
 "assets/NOTICES": "152091bdba666cd0f563dd84be8600df",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/AssetManifest.json": "449823b61151d2e2c9c189dca542cc9b",
-"assets/assets/companies.json": "240d51721e1076f777de47aebba9d646",
+"assets/assets/companies.json": "14cd9bf22816f59ebd73d460bd4baf2a",
 "assets/FontManifest.json": "88c4d131319acabb39aa01e02c9076fa",
 "main.dart.js": "d234d38322757ade6d9ac601c694e030",
 "version.json": "9f93f48b11403cefa8676231ca2af006"
